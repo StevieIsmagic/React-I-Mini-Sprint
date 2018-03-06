9bc97290712815b4c13da1e4b51c2897ef8c0280
Colombianito66 <colombianito66@gmail.com>
2018-03-05 20:07:08 -0500

    App.js steps 4 - 13 complete

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,21 @@ import MoviesList from './MoviesList';
 // Step 4: Write a class called App, extending Component.
 class App extends Component {
   // Step 5: Write a constructor function without passing anything into it.
+  constructor() {
   // Step 6: Inside the constructor function, call super without passing anything into it.
   // Step 7: Inside the constructor function, create a state on this, and set it to an object.
   // Step 8: Inside the object, set a key to movies, and the value to an array of movies
+     super();
+    this.state = { movies = []};    }
   // Step 9: Write a render function.
-  // Step 10: Inside the render function, write a return statement.
-  // Step 11: Inside the return statement, write a selfclosing tag called MoviesList
-  // Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket.
+  render() {
+    // Step 10: Inside the render function, write a return statement.
+    // Step 11: Inside the return statement, write a selfclosing tag called MoviesList
+    // Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket.
+    return (
+      <MoviesList movie={this.state.movies}/>
+    )
+  }  
 }
 // Step 13: Outside the class, export the App class as a default.
 export default App;
